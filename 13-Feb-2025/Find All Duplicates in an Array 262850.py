36ec6211b7bfed24182b7fc2f0e025929d7a77e0
noor-eldien <162597717+noor-eldien@users.noreply.github.com>
2025-02-13 15:02:43 +0200

    Created 'Find All Duplicates in an Array' by A2SV Hub

---
+++ 13-Feb-2025/Find All Duplicates in an Array 262850.py
@@ -0,0 +1,6 @@
+# Problem: Find All Duplicates in an Array - https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        arr = dict(Counter(nums))
+        return [key for key,val in arr.items() if val>1]

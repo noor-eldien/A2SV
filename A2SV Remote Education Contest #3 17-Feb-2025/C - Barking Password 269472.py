753fb54cb9a7cc14d11386317d29fad1245ce79e
noor-eldien <162597717+noor-eldien@users.noreply.github.com>
2025-02-17 19:16:16 +0200

    Created 'C - Barking Password' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/C - Barking Password 269472.py
@@ -0,0 +1,19 @@
+# Problem: C - Barking Password - https://codeforces.com/gym/588094/problem/C
+
+import math,sys,random
+from collections import Counter
+
+ps = input()
+n = int(input())
+lst = []
+for i in range(n):
+    lst.append(input())
+if ps in lst:
+    print("YES")
+    sys.exit()
+for i in range(n):
+    for j in range(n):
+        if lst[i][1] == ps[0] and lst[j][0] == ps[1]:
+            print("YES")
+            sys.exit()
+print("NO")

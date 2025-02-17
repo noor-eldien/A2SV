eb0a145f84674c3ccb8bac98eea053354e205dd6
noor-eldien <162597717+noor-eldien@users.noreply.github.com>
2025-02-17 19:16:21 +0200

    Created 'B - Card Game' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/B - Card Game 269473.py
@@ -0,0 +1,15 @@
+# Problem: B - Card Game - https://codeforces.com/gym/588094/problem/B
+
+import math,sys,random
+from collections import Counter
+
+n = int(input())
+lst = list(map(int, input().split()))
+tmp = sorted(lst)
+val = tmp[0]+tmp[-1]
+for i in range(len(lst)):
+    x = val-lst[i]
+    if x in lst[i+1:]:
+        lst[i] = 0
+        print(i+1,lst.index(x)+1)
+        lst[lst.index(x)] = 0

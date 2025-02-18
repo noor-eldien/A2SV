0535661c4752eda1ed80af90423f7d2340aaea46
noor-eldien <162597717+noor-eldien@users.noreply.github.com>
2025-02-18 15:34:07 +0200

    Created 'Chat Order' by A2SV Hub

---
+++ 18-Feb-2025/Chat Order 271908.py
@@ -0,0 +1,16 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+n = int(input())
+cntr = dict()
+for _ in range(n):
+    s = input()
+    if cntr.get(s,0)>0:
+        cntr.pop(s)
+        cntr[s]=1
+    else:
+        cntr[s]=1
+st =[]
+for k in cntr:
+    st.append(k)
+for i in st[::-1]:
+    print(i)

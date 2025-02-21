97bc2cf6766004d473115a38111d4a74e0791c55
noor-eldien <162597717+noor-eldien@users.noreply.github.com>
2025-02-21 17:53:56 +0200

    Created 'D - Adjacents, ewww !' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 21-Feb-2025/D - Adjacents, ewww ! 278935.py
@@ -0,0 +1,18 @@
+# Problem: D - Adjacents, ewww ! - https://codeforces.com/gym/588094/problem/D
+
+import math,sys,random
+from collections import Counter
+
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    if n==1:
+        print(1)
+    elif n==2:
+        print(-1)
+    elif n>=3:
+        lst = [val for val in range(1,(n*n+1)) if val%2==0 ]
+        lst.extend(val for val in range(1,(n*n+1)) if val%2!=0 )
+        for i in range(0, len(lst), n):
+            print(*lst[i:i + n])

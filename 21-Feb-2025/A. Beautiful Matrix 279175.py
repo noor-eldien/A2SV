25aa7202350660fe3ee72ebe1528882c8298ca46
noor-eldien <162597717+noor-eldien@users.noreply.github.com>
2025-02-21 19:36:26 +0200

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 21-Feb-2025/A. Beautiful Matrix 279175.py
@@ -0,0 +1,19 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+import math,sys,random,copy
+from collections import Counter
+
+
+lst = []
+for _ in range(5):
+    lst.extend(list(map(int,input().split())))
+cntr = 0
+for i in lst:
+    if i == 1:
+        break
+    else:
+        cntr+=1
+
+r = (cntr//5)+1
+c = (cntr%5)+1
+print(abs(3-r)+abs(3-c))
